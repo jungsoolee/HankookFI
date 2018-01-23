cf3b9dcfa6d8ac80e24a1d74f96e5a5ff790f574
jslee <dlshqtm@naver.com>
2018-01-23 10:52:51 +0900

    sdf
    sdf

--- SettleNet/KJYOU/송신_화면/Main.pas
+++ SettleNet/KJYOU/송신_화면/Main.pas
@@ -30,7 +30,7 @@ type
     DRLabel7: TDRLabel;
     DRUserDblCodeCombo1: TDRUserDblCodeCombo;
     DRPanel7: TDRPanel;
-    DRStrGrid_SntFaxTlx: TDRStringGrid;
+    DRStringGrid_Tot_Sent: TDRStringGrid;
     DRPanel4: TDRPanel;
     DRLabel_SntMail: TDRLabel;
     DRSpeedBtn_SntEmailPrint: TDRSpeedButton;
@@ -61,12 +61,26 @@ type
     DRCheckBox1: TDRCheckBox;
     DRRadioButton7: TDRRadioButton;
     DRStringGrid_Tot_Send: TDRStringGrid;
+    DRStringGrid1: TDRStringGrid;
+    DRStringGrid2: TDRStringGrid;
+    DRStringGrid3: TDRStringGrid;
+    DRStringGrid4: TDRStringGrid;
+    DRStringGrid5: TDRStringGrid;
+    DRStringGrid6: TDRStringGrid;
+    DRStringGrid7: TDRStringGrid;
+    DRStringGrid8: TDRStringGrid;
+    DRStringGrid9: TDRStringGrid;
+    DRStringGrid10: TDRStringGrid;
+    DRStringGrid11: TDRStringGrid;
+    DRSplitter1: TDRSplitter;
+    DRBitBtn6: TDRBitBtn;
     procedure FormCreate(Sender: TObject);
     procedure DRStringGrid_Tot_SendDrawCell(Sender: TObject; ACol,
       ARow: Integer; Rect: TRect; State: TGridDrawState);
   private
     { Private declarations }
-    procedure InitStrGrid;
+    procedure InitSendStrGrid(pDRStrGrid: TDRStringGrid);
+    procedure InitSentStrGrid(pDRStrGrid: TDRStringGrid);
     procedure SetStrGrid(pDRStrGrid: TDRStringGrid; pTag: Integer);
   public
     { Public declarations }
@@ -116,29 +130,48 @@ begin
   // 생성시간
   DRMaskEdit1.EditText := '00:00';
 
-  InitStrGrid;
 
   // 전체 StringGrid로 셋팅
+  // 전송대상
   begin
     for i:= 0 to DRPanel8.ControlCount-1 do
     begin
       if DRPanel8.Controls[i] is TDRStringGrid then
       begin
+        InitSendStrGrid(TDRStringGrid(DRPanel8.Controls[i]));
+        SetStrGrid(TDRStringGrid(DRPanel8.Controls[i]), TDRStringGrid(DRPanel8.Controls[i]).Tag);
         TDRStringGrid(DRPanel8.Controls[i]).Align := alClient;
         TDRStringGrid(DRPanel8.Controls[i]).Visible := True;
-        if TDRStringGrid(DRPanel8.Controls[i]).Tag <> 0 then
-          TDRStringGrid(DRPanel8.Controls[i]).Visible := False;
+        if TDRStringGrid(DRPanel8.Controls[i]).Tag <> 1 then
+          TDRStringGrid(DRPanel8.Controls[i]).Visible := False
+          ;
+      end;
+    end;
+  end;
+
+  // 전체 StringGrid로 셋팅
+  // 당일 송신 확인
+  begin
+    for i:= 0 to DRPanel7.ControlCount-1 do
+    begin
+      if DRPanel7.Controls[i] is TDRStringGrid then
+      begin
+        InitSentStrGrid(TDRStringGrid(DRPanel7.Controls[i]));
+        TDRStringGrid(DRPanel7.Controls[i]).Align := alClient;
+        TDRStringGrid(DRPanel7.Controls[i]).Visible := True;
+        if TDRStringGrid(DRPanel7.Controls[i]).Tag <> 0 then
+          TDRStringGrid(DRPanel7.Controls[i]).Visible := False;
       end;
     end;
   end;
 
 end;
 
-procedure TForm1.InitStrGrid;
+procedure TForm1.InitSendStrGrid(pDRStrGrid: TDRStringGrid);
 var
   i: integer;
 begin
-  with DRStringGrid_Tot_Send do
+  with pDRStrGrid do
   begin
     for i:= 1 to 36 do
     begin
@@ -271,6 +304,25 @@ begin
     Cells[8, 34]  := '금융상품 현금매수 확인서 외화RP 신규매수';
     Cells[8, 35]  := '잔고증명서';
     Cells[8, 36]  := '원천징수영수증';
+
+    Cells[9, 1]   := '11:25:12';
+    Cells[9, 2]   := '11:25:12';
+    Cells[9, 3]   := '11:25:12';
+    Cells[9, 4]   := '11:25:12';
+    Cells[9, 5]   := '11:25:12';
+    Cells[9, 6]   := '11:25:12';
+    Cells[9, 7]   := '11:25:12';
+    Cells[9, 8]   := '11:25:12';
+    Cells[9, 9]   := '11:25:12';
+    Cells[9, 10]  := '11:25:12';
+    Cells[9, 11]  := '11:25:12';
+    Cells[9, 12]  := '11:25:12';
+    Cells[9, 13]  := '11:25:12';
+    Cells[9, 14]  := '11:25:12';
+    Cells[9, 15]  := '11:25:12';
+    Cells[9, 16]  := '11:28:44';
+    Cells[9, 17]  := '11:28:44';
+    Cells[9, 20]  := '11:28:44';
   end;
 end;
 
@@ -387,6 +439,178 @@ begin
       TDRStringGrid(Sender).Canvas.FillRect(Rect);
       TDRStringGrid(Sender).Canvas.TextOut(Rect.Left+1, Rect.Top+2, TDRStringGrid(Sender).Cells[ACol, ARow]);
     end;
+  end;
+end;
+
+procedure TForm1.InitSentStrGrid(pDRStrGrid: TDRStringGrid);
+var
+  i: integer;
+begin
+  with pDRStrGrid do
+  begin
+    for i:= 1 to 19 do
+    begin
+      Rows[i].Clear;
+    end;
+
+    Cells[1,  1] := '975001';
+    Cells[1,  2] := '975023';
+
+    Cells[2,  1] := '12345678';
+    Cells[2,  7] := '12345678-12';
+    Cells[2, 13] := '12345678-12-1234';
+    Cells[2, 16] := '00000000-00';
+
+    Cells[3,  1] := '데이터로드_계좌_1';
+    Cells[3,  7] := '데이터로드_계좌_12';
+    Cells[3, 13] := '데이터로드_계좌_1234';
+    Cells[3, 16] := '경남은행_계좌_1';
+
+    Cells[4,  1] := 'FAX';
+    Cells[4,  3] := 'FAX';
+    Cells[4,  5] := 'E-mail';
+    Cells[4,  7] := 'FAX';
+    Cells[4,  9] := 'FAX';
+    Cells[4, 11] := 'E-mail';
+    Cells[4, 13] := 'FAX';
+    Cells[4, 14] := 'FAX';
+    Cells[4, 15] := 'E-mail';
+    Cells[4, 16] := 'FAX';
+    Cells[4, 17] := 'E-mail';
+
+    Cells[5,  1] := '인기필';
+    Cells[5,  3] := '유광진';
+    Cells[5,  5] := '인기필;유광진;';
+    Cells[5,  7] := '인기필';
+    Cells[5,  9] := '유광진';
+    Cells[5, 11] := '인기필;유광진;';
+    Cells[5, 13] := '인기필';
+    Cells[5, 14] := '유광진';
+    Cells[5, 15] := '인기필;유광진;';
+    Cells[5, 16] := '이정수';
+    Cells[5, 17] := '이정수';
+
+    Cells[6,  1] := '0000-0000';
+    Cells[6,  3] := '1234-5678';
+    Cells[6,  5] := 'in@dr.com;yk@dr.com;';
+    Cells[6,  7] := '0000-0000';
+    Cells[6,  9] := '1234-5678';
+    Cells[6, 11] := 'in@dr.com;yk@dr.com;';
+    Cells[6, 13] := '0000-0000';
+    Cells[6, 14] := '1234-5678';
+    Cells[6, 15] := 'in@dr.com;yk@dr.com;';
+    Cells[6, 16] := '9999-9999';
+    Cells[6, 17] := 'js@naver.com;';
+
+    Cells[7,  1] := '1';
+    Cells[7,  2] := '2';
+    Cells[7,  3] := '1';
+    Cells[7,  4] := '2';
+    Cells[7,  5] := '1';
+    Cells[7,  6] := '2';
+    Cells[7,  7] := '1';
+    Cells[7,  8] := '2';
+    Cells[7,  9] := '1';
+    Cells[7, 10] := '2';
+    Cells[7, 11] := '1';
+    Cells[7, 12] := '2';
+    Cells[7, 13] := '1';
+    Cells[7, 14] := '1';
+    Cells[7, 15] := '1';
+    Cells[7, 16] := '1';
+    Cells[7, 17] := '1';
+    Cells[7, 18] := '2';
+
+    Cells[8,  1] := '금융상품 현금매수 확인서 외화RP 신규매수';
+    Cells[8,  2] := '금융상품 현금매수 확인서 외화RP 매도신청';
+    Cells[8,  3] := '금융상품 현금매수 확인서 외화RP 신규매수';
+    Cells[8,  4] := '금융상품 현금매수 확인서 외화RP 매도신청';
+    Cells[8,  5] := '금융상품 현금매수 확인서 외화RP 신규매수';
+    Cells[8,  6] := '금융상품 현금매수 확인서 외화RP 매도신청';
+    Cells[8,  7] := '금융상품 현금매수 확인서 외화RP 매도신청';
+    Cells[8,  8] := '원천징수영수증';
+    Cells[8,  9] := '금융상품 현금매수 확인서 외화RP 매도신청';
+    Cells[8, 10] := '원천징수영수증';
+    Cells[8, 11] := '금융상품 현금매수 확인서 외화RP 매도신청';
+    Cells[8, 12] := '원천징수영수증';
+    Cells[8, 13] := '잔고증명서';
+    Cells[8, 14] := '잔고증명서';
+    Cells[8, 15] := '잔고증명서';
+    Cells[8, 16] := '잔고증명서';
+    Cells[8, 17] := '잔고증명서';
+    Cells[8, 18] := 'RP수익금계산서 RP';
+
+    Cells[9,  1] := '11:25:12';
+    Cells[9,  2] := '11:25:12';
+    Cells[9,  3] := '11:25:12';
+    Cells[9,  4] := '11:25:12';
+    Cells[9,  5] := '11:25:12';
+    Cells[9,  6] := '11:25:12';
+    Cells[9,  7] := '11:25:12';
+    Cells[9,  8] := '11:25:12';
+    Cells[9,  9] := '11:25:12';
+    Cells[9, 10] := '11:25:12';
+    Cells[9, 11] := '11:25:12';
+    Cells[9, 12] := '11:25:12';
+    Cells[9, 13] := '11:25:12';
+    Cells[9, 14] := '11:25:12';
+    Cells[9, 15] := '11:25:12';
+    Cells[9, 16] := '11:28:44';
+    Cells[9, 17] := '11:28:44';
+    Cells[9, 18] := '11:28:44';
+
+    Cells[10,  1] := '11:26:01';
+    Cells[10,  2] := '12:01:23';
+    Cells[10,  3] := '11:25:40';
+    Cells[10,  5] := '11:25:12';
+    Cells[10,  6] := '11:25:12';
+    Cells[10, 11] := '11:25:12';
+    Cells[10, 12] := '11:25:12';
+    Cells[10, 14] := '11:26:01';
+    Cells[10, 15] := '11:25:12';
+    Cells[10, 17] := '11:28:44';
+    Cells[10, 18] := '11:28:44';
+
+    Cells[11,  2] := '50';
+    Cells[11, 13] := '100';
+
+    Cells[12,  1] := 'FINISH';
+    Cells[12,  2] := 'FINISH';
+    Cells[12,  3] := 'FINISH';
+    Cells[12,  4] := 'Sending..';
+    Cells[12,  5] := 'FINISH';
+    Cells[12,  6] := 'FINISH';
+    Cells[12,  7] := 'Sending..';
+    Cells[12,  8] := 'Wating..';
+    Cells[12,  9] := 'Sending..';
+    Cells[12, 10] := 'Sending..';
+    Cells[12, 11] := 'FINISH';
+    Cells[12, 12] := 'FINISH';
+    Cells[12, 13] := 'ERROR';
+    Cells[12, 14] := 'FINISH';
+    Cells[12, 15] := 'FINISH';
+    Cells[12, 16] := 'Sending..';
+    Cells[12, 17] := 'FINISH';
+    Cells[12, 18] := 'FINISH';
+
+    Cells[13,  1] := '1/1';
+    Cells[13,  2] := '2/2';
+    Cells[13,  3] := '1/1';
+    Cells[13,  4] := '1/2';
+    Cells[13,  5] := '-';
+    Cells[13,  6] := '-';
+    Cells[13,  7] := '2/3';
+    Cells[13,  9] := '1/4';
+    Cells[13, 10] := '1/2';
+    Cells[13, 11] := '-';
+    Cells[13, 12] := '-';
+    Cells[13, 13] := '-';
+    Cells[13, 14] := '1/1';
+    Cells[13, 15] := '-';
+    Cells[13, 16] := '1/3';
+    Cells[13, 17] := '-';
+    Cells[13, 18] := '-';
+
   end;
 end;
 
